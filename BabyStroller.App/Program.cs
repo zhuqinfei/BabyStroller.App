8f5f0be2080f7c585c3fe0fecd270cd652f5da7e
zhuqinfei <1586859762@qq.com>
2024-02-02 21:27:25 +0800

    带SDK的系统

--- BabyStroller.App/Program.cs
+++ BabyStroller.App/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using BabyStroller.SDK;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Runtime.Loader;
 
 namespace BabyStroller.App
@@ -18,11 +20,17 @@ namespace BabyStroller.App
                 var types = assembly.GetTypes();
                 foreach (var t in types)
                 {
-                    //这种情况当插件开发商，不小心将Voice写成了voice，那么就会过滤掉方法，
-                    //为了避免插件开发商避免这类错误，主体商就要开发一个SDK给到第三方
-                    if (t.GetMethod("Voice") != null)
+                    //当插件开发商，不小心将Voice写成了voice，那么就会过滤掉方法，
+                    //为了避免这类错误，主体商就要开发一个SDK给到第三方使用，思路：
+                    //就是新建一个SDK类库，里面定义一个IAnimal接口，同时主体商和第三方都要引入
+                    //这个类库SDK文件。UnfinishedAttribute:Attribute这个是为了第三方未完成时
+                    //可以用[Unfinished]确保代码还能正常运行
+                    if (t.GetInterfaces().Contains(typeof(IAnimal)))
                     {
+                        var isUnfinished = t.GetCustomAttributes(false).Any(a => a.GetType() == typeof(UnfinishedAttribute));
+                        if (isUnfinished) continue;
                         animalTypes.Add(t);
+
                     }
                 }
             }
@@ -47,7 +55,8 @@ namespace BabyStroller.App
                 var t = animalTypes[index - 1];
                 var m = t.GetMethod("Voice");
                 var o = Activator.CreateInstance(t);
-                m.Invoke(o, new object[] { times });
+                var a = o as IAnimal;
+                a.Voice(times);
             }
         }
     }
